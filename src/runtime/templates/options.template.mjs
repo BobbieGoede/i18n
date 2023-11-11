6b1730cc3b555caf60c0be459a3aa15d444f722f
Bobbie Goede <bobbiegoede@gmail.com>
2023-11-12 00:41:04 +0100

    fix: nuxt instance outside of plugin

--- src/runtime/templates/options.template.mjs
+++ src/runtime/templates/options.template.mjs
@@ -12,25 +12,46 @@ export const localeMessages = { <% options.localeMessages.forEach(function ([key
   ],<% }); %>
 }
 
+export const vueI18nConfigLoaders = [ 
+  <% options.vueI18nConfigs.forEach(function (importer) { %><%= importer %>,
+    <% }); %>
+]
+
 export const resolveNuxtI18nOptions = async (context) => {
   const nuxtI18nOptions = <%= JSON.stringify(options.nuxtI18nOptions, null, 2) %>
   
-  const vueI18nConfigLoader = async loader => {
-    const config = await loader().then(r => r.default || r)
-    if (typeof config === 'object') return config
-    if (typeof config === 'function') return await config()
-    return {}
-  }
-    
-  const mergeVueI18nConfigs = async (loader) => {
-    const layerConfig = await vueI18nConfigLoader(loader)
-    nuxtI18nOptions.vueI18n = defu(nuxtI18nOptions.vueI18n, layerConfig || {})
-  }
+  // const vueI18nConfigLoader = async loader => {
+  //   const config = await loader().then(r => r.default || r)
+  //   if (typeof config === 'object') return config
+  //   if (typeof config === 'function') return await config()
+  //   return {}
+  // }
 
-  nuxtI18nOptions.vueI18n = { messages: {} }
-  <% options.vueI18nConfigs.forEach(function (importer) { %>
-  await mergeVueI18nConfigs(<%= importer %>) <% }); %>
-    
+  // const mergeVueI18nConfigs = async (loader) => {
+  //   const layerConfig = await vueI18nConfigLoader(loader)
+  //   nuxtI18nOptions.vueI18n = defu(nuxtI18nOptions.vueI18n, layerConfig || {})
+  // }
+
+  // <% options.vueI18nConfigs.forEach(function (importer) { %>
+  // await mergeVueI18nConfigs(<%= importer %>) <% }); %>
+  
+  // nuxtI18nOptions.vueI18n = { messages: { } }
+  // <% options.vueI18nConfigs.forEach(function (importer, i) { %>
+  //   const { default: resolver<%=i%> } = await context.runWithContext(<%= importer %>);
+  //   const resolved<%=i%> = typeof resolver<%=i%> === 'function' ? resolver<%=i%>() : resolver<%=i%>
+  //   nuxtI18nOptions.vueI18n = defu(nuxtI18nOptions.vueI18n, resolved<%=i%>)
+  // <% }); %>
+
+  // for (const vLoader of vueI18nConfigLoaders) {
+  //   const { default: resolver } = await vLoader()
+  //   const resolved = typeof resolver === 'function' ? resolver() : resolver
+
+  //   // console.log(resolved)
+  //   // @ts-ignore
+  //   nuxtI18nOptions.vueI18n = defu(nuxtI18nOptions.vueI18n, resolved)
+  // }
+  // console.log(nuxtI18nOptions.vueI18n)
+  
   return nuxtI18nOptions
 }
 
