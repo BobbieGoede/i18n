e7c432cf538ba192d570c1e6542132ec30fef222
kazuya kawaguchi <kawakazu80@gmail.com>
2024-07-03 17:53:57 +0900

    fix

--- specs/issues/2226.spec.ts
+++ specs/issues/2226.spec.ts
@@ -9,6 +9,7 @@ describe('#2226', async () => {
   })
 
   test('navigate on `prefix_and_default`', async () => {
+    const waitTime = import.meta.env.CI ? 50 : 5
     const home = url('/')
     const page = await createPage()
     await page.goto(home)
@@ -17,16 +18,16 @@ describe('#2226', async () => {
     expect(await getText(page, '#default-locale')).include(`Default locale: false`)
 
     await page.locator('#goto-about').click()
-    await waitForMs(5)
+    await waitForMs(waitTime)
     expect(await getText(page, '#content')).include(`This is about page. To home page`)
 
     await page.locator('#goto-index').click()
-    await waitForMs(5)
+    await waitForMs(waitTime)
     expect(await getText(page, '#content')).include(`This is home page. To about pag`)
 
     await page.locator('#lang-switch').click()
     await page.locator('#goto-about').click()
-    await waitForMs(5)
+    await waitForMs(waitTime)
     expect(await getText(page, '#content')).include(`This is about page. To home page`)
   })
 })
